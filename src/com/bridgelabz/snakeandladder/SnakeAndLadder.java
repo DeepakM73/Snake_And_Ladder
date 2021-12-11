31afe96bec22191a42ef54a4d79aed6d2ec521a1
DeepakM73 <deepak.muddasani73@gmail.com>
2021-12-11 07:48:34 +0530

    Added: Dice

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -4,6 +4,9 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
         int start=0;
+        System.out.println("Welcome to Snake And Ladder game");
         System.out.println("\nStarting the game with Point= " + start);
+        int dice = (int)(Math.random() * 6) + 1;
+        System.out.println("number on the dice is :" + dice);
     }
 }
